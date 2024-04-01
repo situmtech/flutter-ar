f44aa47e74a4faab49a0a7562636491f6d4cfd06
German Rodriguez <german.rodriguez@situm.com>
2024-04-01 12:29:27 +0200

    fix

--- lib/ar_pos_quality.dart
+++ lib/ar_pos_quality.dart
@@ -27,7 +27,7 @@ class _ARPosQualityState extends State<_ARPosQuality> {
 
   bool showARAlertWidget = true;
 
-  int refreshData = ARModeDebugValues.dynamicUnstableRefreshTime;
+  int refreshData = ARModeDebugValues.dynamicUnstableRefreshTime.value;
   bool hasToRefresh = true;
   int waitToRefreshTimer = 0;
   int keepRefreshingTimer = 0;
@@ -146,7 +146,7 @@ class _ARPosQualityState extends State<_ARPosQuality> {
   void updateDynamicARParams(List<Location> locations) {
     if (locations.length < ARModeDebugValues.locationBufferSize.value ||
         !this.allLocationsInSameFloor(locations)) {
-      this.refreshData = ARModeDebugValues.dynamicUnstableRefreshTime;
+      this.refreshData = ARModeDebugValues.dynamicUnstableRefreshTime.value;
       distanceWalked = 0;
       return;
     }
@@ -174,7 +174,8 @@ class _ARPosQualityState extends State<_ARPosQuality> {
   }
 
   bool isRefreshing() {
-    return this.refreshData == ARModeDebugValues.dynamicUnstableRefreshTime;
+    return this.refreshData ==
+        ARModeDebugValues.dynamicUnstableRefreshTime.value;
   }
 
   void updateWaitToRefreshTimer() {
@@ -205,14 +206,14 @@ class _ARPosQualityState extends State<_ARPosQuality> {
       hasToRefresh = false;
     } else if (hasToRefresh &&
         waitToRefreshTimer == ARModeDebugValues.dynamicTimeToRefresh.value) {
-      this.refreshData = ARModeDebugValues.dynamicUnstableRefreshTime;
+      this.refreshData = ARModeDebugValues.dynamicUnstableRefreshTime.value;
       waitToRefreshTimer = 0;
       hasToRefresh = false;
     }
   }
 
   void forceResetRefreshTimers() {
-    this.refreshData = ARModeDebugValues.dynamicUnstableRefreshTime;
+    this.refreshData = ARModeDebugValues.dynamicUnstableRefreshTime.value;
     keepRefreshingTimer = 0;
     hasToRefresh = true;
   }
