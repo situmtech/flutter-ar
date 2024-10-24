0fa674d95bed68f09cf218eb301e0f9f8f9c5f5b
german <german.rodriguez@situm.com>
2024-10-24 10:29:20 +0200

    Set config panel and debug info.

--- ios/Classes/ARView/ConfigDebug.swift
+++ ios/Classes/ARView/ConfigDebug.swift
@@ -11,6 +11,7 @@ class ConfigDebug {
     var infoLabel: UILabel?
     var isInfoVisible = false
     var refreshTimer: Timer?
+    
     var infoLabel1: UILabel?
     var infoLabel2: UILabel?
     var infoLabel3: UILabel?
@@ -27,6 +28,14 @@ class ConfigDebug {
     var cameraDeph = 20
     var arrowDistance = 5
     
+    var configStackView: UIStackView?
+    var infoStackView: UIStackView?
+    var mainStackView: UIStackView?
+    
+    let expandedSpacing: CGFloat = 20
+    let collapsedSpacing: CGFloat = -180
+
+    
     init(arQuality: ARQuality?, hasToRefresh: Bool) {
             self.arQuality = arQuality
             self.hasToRefresh = hasToRefresh
@@ -34,121 +43,139 @@ class ConfigDebug {
 
     
     // Función para crear el botón de Toggle Info
-        func setupUpdateDebugInfo(view: UIView) {
-            // Crear el botón
-            updateButton = UIButton(type: .system)
-            updateButton?.setTitle("Info Debug", for: .normal)
-            updateButton?.frame = CGRect(x: 20, y: 20, width: 100, height: 30)
-            updateButton?.backgroundColor = .systemBlue
-            updateButton?.setTitleColor(.white, for: .normal)
-            updateButton?.layer.cornerRadius = 10
-
-            // Añadir la acción del botón
-            updateButton?.addTarget(self, action: #selector(toggleInfoDebug), for: .touchUpInside)
-
-            // Añadir el botón a la vista principal
-            if let button = updateButton {
-                view.addSubview(button)
-            }
+    func setupUpdateDebugInfo(view: UIView) {
+        updateButton = UIButton(type: .system)
+        updateButton?.setTitle("Info Debug", for: .normal)
+        updateButton?.backgroundColor = .systemBlue
+        updateButton?.setTitleColor(.white, for: .normal)
+        updateButton?.layer.cornerRadius = 10
+        updateButton?.frame = CGRect(x: 20, y: 20, width: 100, height: 30)
+        updateButton?.addTarget(self, action: #selector(toggleInfoDebug), for: .touchUpInside)
+        
+        if let button = updateButton {
+            view.addSubview(button)
         }
+    }
 
         // Crear el panel de información y configuración
     func setupInfoPanel(view: UIView) {
- 
-            infoPanel = UIView(frame: CGRect(x: 10, y: 50, width: view.frame.width - 40, height: 350))
-            infoPanel?.backgroundColor = .clear
-            infoPanel?.layer.cornerRadius = 10
-            infoPanel?.layer.borderWidth = 2
-            infoPanel?.layer.borderColor = UIColor.lightGray.cgColor
-
-            infoLabel1 = UILabel(frame: CGRect(x: 10, y: 10, width: infoPanel!.frame.width - 20, height: 20))
-            infoLabel1?.textAlignment = .left
-            infoLabel1?.textColor = .gray
-            infoPanel?.addSubview(infoLabel1!)
-
-            infoLabel2 = UILabel(frame: CGRect(x: 10, y: 30, width: infoPanel!.frame.width - 20, height: 20))
-            infoLabel2?.textAlignment = .left
-            infoLabel2?.textColor = .gray
-            infoPanel?.addSubview(infoLabel2!)
-
-            infoLabel3 = UILabel(frame: CGRect(x: 10, y: 50, width: infoPanel!.frame.width - 20, height: 20))
-            infoLabel3?.textAlignment = .left
-            infoLabel3?.textColor = .gray
-            infoPanel?.addSubview(infoLabel3!)
-
-            infoLabel4 = UILabel(frame: CGRect(x: 10, y: 70, width: infoPanel!.frame.width - 20, height: 20))
-            infoLabel4?.textAlignment = .left
-            infoLabel4?.textColor = .gray
-            infoPanel?.addSubview(infoLabel4!)
-
-            infoLabel5 = UILabel(frame: CGRect(x: 10, y: 90, width: infoPanel!.frame.width - 20, height: 20))
-            infoLabel5?.textAlignment = .left
-            infoLabel5?.textColor = .gray
-            infoPanel?.addSubview(infoLabel5!)
-
-            // Sección de configuración
-        
-            // Sección de configuración
-            let configLabel = UILabel(frame: CGRect(x: 10, y: 120, width: 200, height: 20))
-            configLabel.text = "Activar Configuración:"
-            configLabel.textColor = .gray
-            infoPanel?.addSubview(configLabel)
-
-            let configSwitch = UISwitch(frame: CGRect(x: infoPanel!.frame.width - 70, y: 120, width: 50, height: 30))
-            configSwitch.isOn = false
-            configSwitch.addTarget(self, action: #selector(configSwitchChanged(_:)), for: .valueChanged)
-            infoPanel?.addSubview(configSwitch)
-
-            // Campos de configuración (ocultos inicialmente)
-            configTextField1 = UITextField(frame: CGRect(x: 10, y: 150, width: infoPanel!.frame.width - 20, height: 30))
-            configTextField1?.placeholder = "Quality decrease"
-            configTextField1?.borderStyle = .roundedRect
-            configTextField1?.isHidden = true
-            configTextField1?.text = String(qualityDecrease)
-            configTextField1?.isUserInteractionEnabled = true
-            configTextField1?.isEnabled = true
-            configTextField1?.addTarget(self, action: #selector(configTextFieldDidChange(_:)), for: .editingChanged)
-            infoPanel?.addSubview(configTextField1!)
-
-            configTextField2 = UITextField(frame: CGRect(x: 10, y: 190, width: infoPanel!.frame.width - 20, height: 30))
-            configTextField2?.placeholder = "Threshold decrease"
-            configTextField2?.borderStyle = .roundedRect
-            configTextField2?.isHidden = true
-            configTextField2?.text = String(thresholdDecrease)
-            configTextField2?.isUserInteractionEnabled = true
-            configTextField2?.isEnabled = true
-            configTextField2?.addTarget(self, action: #selector(configTextFieldDidChange(_:)), for: .editingChanged)
-            infoPanel?.addSubview(configTextField2!)
-        
-            configTextField3 = UITextField(frame: CGRect(x: 10, y: 230, width: infoPanel!.frame.width - 20, height: 30))
-            configTextField3?.placeholder = "Camera deph"
-            configTextField3?.borderStyle = .roundedRect
-            configTextField3?.isHidden = true
-            configTextField3?.text = String(cameraDeph)
-            configTextField3?.isEnabled = true
-            configTextField3?.isUserInteractionEnabled = true
-            configTextField3?.addTarget(self, action: #selector(configTextFieldDidChange(_:)), for: .editingChanged)
-            infoPanel?.addSubview(configTextField3!)
-            
-            configTextField4 = UITextField(frame: CGRect(x: 10, y: 270, width: infoPanel!.frame.width - 20, height: 30))
-            configTextField4?.placeholder = "Arrow distance"
-            configTextField4?.borderStyle = .roundedRect
-            configTextField4?.isHidden = true
-            configTextField4?.text = String(arrowDistance)
-            configTextField4?.isEnabled = true
-            configTextField4?.isUserInteractionEnabled = true
-            configTextField4?.addTarget(self, action: #selector(configTextFieldDidChange(_:)), for: .editingChanged)
-            infoPanel?.addSubview(configTextField4!)
-        
-        
-            
-            if let panel = infoPanel {
-                panel.isHidden = true
-                view.addSubview(panel)
- 
-            }
-        
-        }
+         infoPanel = UIView()
+         infoPanel?.translatesAutoresizingMaskIntoConstraints = false
+         infoPanel?.backgroundColor = .clear
+         infoPanel?.layer.cornerRadius = 10
+         infoPanel?.layer.borderWidth = 2
+         infoPanel?.layer.borderColor = UIColor.lightGray.cgColor
+         
+         // Crear la vista de configuración
+         configTextField1 = createTextField(placeholder: "Quality decrease", value: String(qualityDecrease))
+         configTextField2 = createTextField(placeholder: "Threshold decrease", value: String(thresholdDecrease))
+         configTextField3 = createTextField(placeholder: "Camera depth", value: String(cameraDeph))
+         configTextField4 = createTextField(placeholder: "Arrow distance", value: String(arrowDistance))
+         
+         // Organizar los campos de configuración en un UIStackView
+         configStackView = UIStackView(arrangedSubviews: [configTextField1!, configTextField2!, configTextField3!, configTextField4!])
+         configStackView?.axis = .vertical
+         configStackView?.spacing = 10
+         configStackView?.alignment = .fill
+         
+         // Crear la parte superior de configuración con un switch
+         let configView = UIView()
+         let configLabel = UILabel()
+         configLabel.text = "Activar Configuración:"
+         configLabel.textColor = .black
+         
+         let configSwitch = UISwitch()
+         configSwitch.isOn = false
+         configSwitch.addTarget(self, action: #selector(configSwitchChanged(_:)), for: .valueChanged)
+         
+         let configHeaderStackView = UIStackView(arrangedSubviews: [configLabel, configSwitch])
+         configHeaderStackView.axis = .horizontal
+         configHeaderStackView.spacing = 10
+         configHeaderStackView.alignment = .center
+         
+         // Agregar la cabecera y campos de configuración al configView
+         configView.addSubview(configHeaderStackView)
+         configView.addSubview(configStackView!)
+         
+         // Ajustar el layout con Auto Layout
+         configHeaderStackView.translatesAutoresizingMaskIntoConstraints = false
+         configStackView?.translatesAutoresizingMaskIntoConstraints = false
+         
+         NSLayoutConstraint.activate([
+             configHeaderStackView.topAnchor.constraint(equalTo: configView.topAnchor, constant: 10),
+             configHeaderStackView.leadingAnchor.constraint(equalTo: configView.leadingAnchor, constant: 10),
+             configHeaderStackView.trailingAnchor.constraint(equalTo: configView.trailingAnchor, constant: -10),
+             
+             configStackView!.topAnchor.constraint(equalTo: configHeaderStackView.bottomAnchor, constant: 10),
+             configStackView!.leadingAnchor.constraint(equalTo: configView.leadingAnchor, constant: 10),
+             configStackView!.trailingAnchor.constraint(equalTo: configView.trailingAnchor, constant: -10),
+             configStackView!.bottomAnchor.constraint(equalTo: configView.bottomAnchor, constant: -10)
+         ])
+         
+         // Ocultar configuración inicialmente
+         configStackView?.isHidden = true
+         
+         // Crear las etiquetas de información
+         infoLabel1 = UILabel()
+         infoLabel2 = UILabel()
+         infoLabel3 = UILabel()
+         infoLabel4 = UILabel()
+         infoLabel5 = UILabel()
+         
+         // Configurar las etiquetas
+         [infoLabel1, infoLabel2, infoLabel3, infoLabel4, infoLabel5].forEach { label in
+             label?.textAlignment = .left
+             label?.textColor = .black
+         }
+         
+         // Organizar las etiquetas de información en un UIStackView
+         infoStackView = UIStackView(arrangedSubviews: [infoLabel1!, infoLabel2!, infoLabel3!, infoLabel4!, infoLabel5!])
+         infoStackView?.axis = .vertical
+         infoStackView?.spacing = 5
+         infoStackView?.alignment = .fill
+         
+         // Crear el StackView principal que contiene la configuración y la información
+         mainStackView = UIStackView(arrangedSubviews: [configView, infoStackView!])
+         mainStackView?.axis = .vertical
+         mainStackView?.spacing = collapsedSpacing // Espaciado inicial cuando la configuración está oculta
+         mainStackView?.translatesAutoresizingMaskIntoConstraints = false
+         
+         // Agregar el StackView principal a la vista infoPanel
+         infoPanel?.addSubview(mainStackView!)
+         
+         // Configurar restricciones para el mainStackView
+         NSLayoutConstraint.activate([
+             mainStackView!.leadingAnchor.constraint(equalTo: infoPanel!.leadingAnchor, constant: 10),
+             mainStackView!.trailingAnchor.constraint(equalTo: infoPanel!.trailingAnchor, constant: -10),
+             mainStackView!.topAnchor.constraint(equalTo: infoPanel!.topAnchor, constant: 10),
+             mainStackView!.bottomAnchor.constraint(equalTo: infoPanel!.bottomAnchor, constant: -10)
+         ])
+         
+         // Agregar infoPanel a la vista principal
+         if let panel = infoPanel {
+             view.addSubview(panel)
+             
+             // Configurar restricciones para infoPanel
+             NSLayoutConstraint.activate([
+                 panel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+                 panel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+                 panel.topAnchor.constraint(equalTo: updateButton!.bottomAnchor, constant: 10)
+             ])
+         }
+     }
+           
+      
+    private func createTextField(placeholder: String, value: String) -> UITextField {
+        let textField = UITextField()
+        textField.placeholder = placeholder
+        textField.borderStyle = .roundedRect
+        textField.text = value
+        textField.isUserInteractionEnabled = true
+        textField.isEnabled = true
+        textField.addTarget(self, action: #selector(configTextFieldDidChange(_:)), for: .editingChanged)
+        return textField
+    }
+    
     func setParametersUpdated(){
         arQuality?.setQualityDecrease(qualityDecrease: Float(qualityDecrease))
         arQuality?.setThresholdDecrease(thresholdDecrease: Float(thresholdDecrease))
@@ -204,48 +231,31 @@ class ConfigDebug {
         }
     }
 
+    @objc func configSwitchChanged(_ sender: UISwitch) {
+        configStackView?.isHidden = !sender.isOn
+        mainStackView?.spacing = sender.isOn ? expandedSpacing : collapsedSpacing
+    }
 
 
         // Función que se llama cuando se cambia el valor del switch de configuración
-    @objc func configSwitchChanged(_ sender: UISwitch) {
-            if sender.isOn {
-                configTextField1?.isHidden = false
-                configTextField2?.isHidden = false
-                configTextField3?.isHidden = false
-                configTextField4?.isHidden = false
-                infoPanel?.frame.size.height = 350
-                
-            } else {
-                configTextField1?.isHidden = true
-                configTextField2?.isHidden = true
-                configTextField3?.isHidden = true
-                configTextField4?.isHidden = true
-                infoPanel?.frame.size.height = 150
-            }
-        }
-
-    // Función para alternar entre mostrar y ocultar el panel
     @objc func toggleInfoDebug() {
-        if let panel = infoPanel {
-            panel.isHidden = !panel.isHidden // Alterna la visibilidad
+            guard let panel = infoPanel else { return }
+            panel.isHidden.toggle()
             isInfoVisible.toggle()
         }
-    }
 
-    // Función para iniciar el refresco de la información en tiempo real
-    func startRefreshingInfo() {
-        refreshTimer?.invalidate()
-        refreshTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateAll), userInfo: nil, repeats: true)
-    }
-    
-    @objc func updateAll() {
-         setParametersUpdated()
-         updateInfoPanel()
-     }
+        // Función para iniciar el refresco de la información en tiempo real
+        func startRefreshingInfo() {
+            refreshTimer?.invalidate()
+            refreshTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateAll), userInfo: nil, repeats: true)
+        }
 
+        @objc func updateAll() {
+            setParametersUpdated()
+            updateInfoPanel()
+        }
         // Función que actualiza la información mostrada en el panel
     @objc func updateInfoPanel() {
-        
         guard let arQuality = arQuality else {
             print("Error: arQuality es nil")
             return
@@ -254,23 +264,36 @@ class ConfigDebug {
         // Obtener la información actualizada de arQuality
         let infoDebug = arQuality.getInfoParameters()
         
-        guard let globalQuality = infoDebug["globalQuality"] as? Double else {
-            print("Error: global quality es nil o no es un Double")
-            return
+        if let globalQuality = infoDebug["globalQuality"] as? Double {
+            let roundedQuality = (globalQuality * 100).rounded() / 100
+            
+            // Actualizar las etiquetas con los nuevos valores, desenvolviendo opcionales
+            infoLabel1?.text = "HasToRefresh: \(hasToRefresh)"
+            infoLabel2?.text = "GlobalQuality: \(roundedQuality)"
+            
+            // Asegúrate de desenvolver correctamente las variables opcionales
+            if let dynamicRefreshThreshold = infoDebug["DynamicRefreshThreshold"] {
+                infoLabel3?.text = "DynamicRefreshThreshold: \(dynamicRefreshThreshold)"
+            } else {
+                infoLabel3?.text = "DynamicRefreshThreshold: N/A"
+            }
+            
+            if let arConf = infoDebug["arConf"] {
+                infoLabel4?.text = "ArConf: \(arConf)"
+            } else {
+                infoLabel4?.text = "ArConf: N/A"
+            }
+            
+            if let situmConf = infoDebug["situmConf"] {
+                infoLabel5?.text = "SitumConf: \(situmConf)"
+            } else {
+                infoLabel5?.text = "SitumConf: N/A"
+            }
+        } else {
+            infoLabel2?.text = "GlobalQuality: N/A"
         }
-        
-        let roundedQuality = (globalQuality * 100).rounded() / 100
+    }
 
-        // Actualizar las etiquetas con los nuevos valores
-        infoLabel1?.text = "HasToRefresh: \(hasToRefresh)"
-        infoLabel2?.text = "GlobalQuality: \(roundedQuality)"
-        infoLabel3?.text = "DynamicRefreshThreshold: \(infoDebug["DynamicRefreshThreshold"])"
-        infoLabel4?.text = "ArConf: \(infoDebug["arConf"])"
-        infoLabel5?.text = "SitumConf: \(infoDebug["situmConf"])"
-            
-            
-            
-        }
 
         // Detener el refresco cuando no sea necesario
         func stopRefreshingInfo() {
